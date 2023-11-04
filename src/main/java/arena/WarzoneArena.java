f0062240f0aa4fb84ba9250157eebe6cf6b57ad5
victor-praise <61267951+victor-praise@users.noreply.github.com>
2023-11-04 16:40:19 -0400

    added observer

--- src/main/java/arena/WarzoneArena.java
+++ src/main/java/arena/WarzoneArena.java
@@ -1,5 +1,8 @@
 package main.java.arena;
 
+import main.java.utils.logger.LogEntryBuffer;
+import main.java.utils.logger.LogWriter;
+
 /**
  * A tactical game-play where the player objective is to capture as much territory as possible.
  * Player wins by controlling every territory possible.
@@ -15,6 +18,10 @@ public class WarzoneArena {
     private static Game d_Game;
 
     /**
+     * Global level Logger (Observable)
+     */
+    private static LogEntryBuffer d_Logger;
+    /**
      * Main entry point of the game.
      *
      * @param args Any command line arguments separated by a space
@@ -29,6 +36,8 @@ public class WarzoneArena {
      */
     private static void startGame() {
         d_Game = Game.sharedInstance();
+        d_Logger = LogEntryBuffer.getInstance();
+        d_Logger.addObserver(new LogWriter());
         d_Game.start();
     }
 }
