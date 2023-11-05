704c49219b93161201b73f7e540e6f1d7d9293c0
sha1ksadiq <sadiq2001ali@gmail.com>
2023-11-05 11:35:44 +0530

    added AdvanceCommand validation functionality and 1 test case

--- src/main/java/commands/AdvanceCommand.java
+++ src/main/java/commands/AdvanceCommand.java
@@ -32,16 +32,16 @@ public class AdvanceCommand extends PlayerOrderCommand{
         }
 
         try {
-            String l_sourceCountry = d_baseParams[0];
+            String l_countryName = d_baseParams[0];
             String l_destinationCountry = d_baseParams[1];
             int l_armyUnits = Integer.parseInt(d_baseParams[2]);
             Map map = Game.sharedInstance().getD_map();
-            Country l_ownedSourceCountry = map.getCountry(l_sourceCountry);
+            Country l_ownedSourceCountry = this.d_issuingPlayer.getOwnedCountryWithName((l_countryName));
             Country l_targetDestinationCountry = map.getCountry(l_destinationCountry);
 
             // check ownership of country
             if(l_ownedSourceCountry == null) {
-                l_message = "[AdvanceOrder]: " +d_issuingPlayer.getD_name() + " does not have ownership over country " +l_sourceCountry;
+                l_message = "[AdvanceOrder]: " +d_issuingPlayer.getD_name() + " does not have ownership over country " +l_ownedSourceCountry;
                 LogEntryBuffer.getInstance().log(l_message);
                 System.out.println(l_message);
                 return;
